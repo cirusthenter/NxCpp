08e0ea0eec5ccda734f450ed47447bbc64f9f21b
Cirus Thenter <cirusthenter@gmail.com>
2022-01-02 10:26:59 +0900

    1000000 -> 1048576

--- src/algorithms/centrality/tests/test_betweenness.cc
+++ src/algorithms/centrality/tests/test_betweenness.cc
@@ -4,11 +4,11 @@
 #include "../betweenness.hpp"
 #include <gtest/gtest.h>
 
-bool equal_to_4th_place(double a, double b)
+bool equal_to_6th_place(double a, double b)
 {
     // This has to be updated
-    int x1 = (a * 10000);
-    int x2 = (b * 10000);
+    int x1 = (a * 1048576); // 1048576 = 2^20
+    int x2 = (b * 1048576); // 1048576 = 2^20
     return x1 == x2;
 }
 
