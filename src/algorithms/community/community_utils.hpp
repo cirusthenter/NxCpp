249afe209d449c379d265f14b90dc8b8ac561dbb
Cirus Thenter <cirusthenter@gmail.com>
2022-01-05 01:29:44 +0900

    louvain beta

--- src/algorithms/community/community_utils.hpp
+++ src/algorithms/community/community_utils.hpp
@@ -9,12 +9,18 @@ bool is_partition(Graph g, vector<unordered_set<int>> communities)
         for (auto n : c)
             if (in(nodes, n))
                 nodes_in_communities.insert(n);
+            else
+                return false;
     if (nodes.size() != nodes_in_communities.size())
         return false;
     int number_of_nodes_in_all_communities = 0;
     for (auto c : communities)
         number_of_nodes_in_all_communities += c.size();
-    if (nodes.size() != number_of_nodes_in_all_communities)
+    if (nodes.size() != number_of_nodes_in_all_communities) {
+        cout << "nodes.size() != number_of_nodes_in_all_communities" << endl;
+        cout << "nodes.size() = " << nodes.size() << endl;
+        cout << "number_of_nodes_in_all_communities = " << number_of_nodes_in_all_communities << endl;
         return false;
+    }
     return true;
 }
