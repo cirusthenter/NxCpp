900a12e659b41e1b307acf415aa54618e975a4e1
Cirus Thenter <cirusthenter@gmail.com>
2022-01-27 23:52:08 +0900

    added karate club network

--- readwrite/tests/test_read_file.cc
+++ readwrite/tests/test_read_file.cc
@@ -1,5 +1,5 @@
-#include "../../generators/social.hpp"
 #include "../../algorithms/isomorphism/isomorph.hpp"
+#include "../../generators/social.hpp"
 #include "../read_file.hpp"
 #include <gtest/gtest.h>
 
@@ -8,7 +8,7 @@ class TestReadFile : public ::testing::Test {
 
 TEST_F(TestReadFile, TestReadUndirected)
 {
-    string path = "graph/karate_club.gr";
+    string path = "graphs/karate_club.gr";
     Graph g = read_undirected_file(path);
     Graph karate = karate_club_graph();
 
