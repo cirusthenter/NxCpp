ab3388ee836898120faf1ca6f323838680b09a64
Cirus Thenter <cirusthenter@gmail.com>
2022-01-27 23:23:08 +0900

    remove

--- src/algorithms/community/tests/test_louvain.cc
+++ src/algorithms/community/tests/test_louvain.cc
@@ -34,6 +34,5 @@ TEST_F(TestLouvain, TestPartition)
     Graph g = karate_club_graph();
     vector<unordered_set<int>> partition = louvain_communities(g, "weight", 1, .00000001, true, 0);
 
-    cout << "modularity: " << modularity(g, partition) << endl;
     ASSERT_TRUE(is_partition(g, partition));
 }
